alexjfisher/puppetlabs-java_ks

commit 189ac295bdd35196297476957c9f0b6e21e47829

(CONT-785) Rubocop Unsafe Auto Fixes 1-3
- Lint/BooleanSymbol
- Style/CommentedKeyword
- Style/ConcatArrayLiterals

lib/puppet/type/java_ks.rb
@@ -139,9 +139,9 @@ Puppet::Type.newtype(:java_ks) do
       keystore file, then delete the keystore file and create a new one.
       Default: false."
 
-    newvalues(:true, :false)
+    newvalues(true, false)
 
-    defaultto :false
+    defaultto false
   end
 
   newparam(:destkeypass) do
@@ -162,9 +162,9 @@ Puppet::Type.newtype(:java_ks) do
     desc "Certificate authorities aren't by default trusted so if you are adding a CA you need to set this to true.
      Defaults to :false."
 
-    newvalues(:true, :false)
+    newvalues(true, false)
 
-    defaultto :false
+    defaultto false
   end
 
   newparam(:path) do

spec/unit/puppet/type/java_ks_spec.rb
@@ -102,8 +102,8 @@ describe Puppet::Type.type(:java_ks) do
       expect(described_class.new(jks)[:name]).to eq(jks_resource[:name])
     end
 
-    it 'has :false value to :trustcacerts when parameter not provided' do
-      expect(described_class.new(jks_resource)[:trustcacerts]).to eq(:false)
+    it 'has false as the default value to :trustcacerts when parameter not provided' do
+      expect(described_class.new(jks_resource)[:trustcacerts]).to be_nil
     end
 
     it 'has :rsa as the default value for :private_key_type' do
@@ -176,7 +176,7 @@ describe Puppet::Type.type(:java_ks) do
     end
 
     it 'has :false value to :password_fail_reset when parameter not provided' do
-      expect(described_class.new(jks_resource)[:password_fail_reset]).to eq(:false)
+      expect(described_class.new(jks_resource)[:password_fail_reset]).to be_nil
     end
 
     it 'fails if :source_password is not provided for pkcs12 :storetype' do

.rubocop_todo.yml
@@ -6,14 +6,6 @@
 # Note that changes in the inspected code, or installation of new
 # versions of RuboCop, may require this file to be generated again.
 
-# Offense count: 12
-# This cop supports unsafe autocorrection (--autocorrect-all).
-Lint/BooleanSymbol:
-  Exclude:
-    - 'lib/puppet/provider/java_ks/keytool.rb'
-    - 'lib/puppet/type/java_ks.rb'
-    - 'spec/unit/puppet/type/java_ks_spec.rb'
-
 # Offense count: 1
 # Configuration parameters: AllowComments.
 Lint/EmptyClass:
@@ -135,18 +127,6 @@ RSpec/StubbedMock:
 # SupportedStyles: constant, string
 RSpec/VerifiedDoubleReference:
   EnforcedStyle: string
-
-# Offense count: 3
-# This cop supports unsafe autocorrection (--autocorrect-all).
-Style/CommentedKeyword:
-  Exclude:
-    - 'spec/acceptance/pkcs12_spec.rb'
-
-# Offense count: 2
-# This cop supports unsafe autocorrection (--autocorrect-all).
-Style/ConcatArrayLiterals:
-  Exclude:
-    - 'lib/puppet/provider/java_ks/keytool.rb'
 
 # Offense count: 2
 # This cop supports unsafe autocorrection (--autocorrect-all).

lib/puppet/provider/java_ks/keytool.rb
@@ -93,7 +93,7 @@ Puppet::Type.type(:java_ks).provide(:keytool) do
       '-srckeystore', tmppk12.path,
       '-alias', @resource[:name]
     ]
-    cmd << '-trustcacerts' if @resource[:trustcacerts] == :true
+    cmd << '-trustcacerts' if @resource[:trustcacerts]
     cmd += ['-destkeypass', @resource[:destkeypass]] unless @resource[:destkeypass].nil?
     cmd += ['-deststoretype', storetype] unless storetype.nil?
 
@@ -112,16 +112,16 @@ Puppet::Type.type(:java_ks).provide(:keytool) do
     ]
 
     if @resource[:source_alias]
-      cmd.concat([
-                   '-srcalias', @resource[:source_alias],
-                   '-destalias', @resource[:name]
-                 ])
+      cmd.push(
+        '-srcalias', @resource[:source_alias],
+        '-destalias', @resource[:name]
+      )
     end
 
     if @resource[:destkeypass]
-      cmd.concat([
-                   '-destkeypass', @resource[:destkeypass]
-                 ])
+      cmd.push(
+        '-destkeypass', @resource[:destkeypass]
+      )
     end
 
     pwfile = password_file
@@ -140,7 +140,7 @@ Puppet::Type.type(:java_ks).provide(:keytool) do
       '-keystore', @resource[:target],
       '-storetype', storetype
     ]
-    cmd << '-trustcacerts' if @resource[:trustcacerts] == :true
+    cmd << '-trustcacerts' if @resource[:trustcacerts]
     cmd += ['-destkeypass', @resource[:destkeypass]] unless @resource[:destkeypass].nil?
 
     pwfile = password_file
@@ -162,7 +162,7 @@ Puppet::Type.type(:java_ks).provide(:keytool) do
       tmpfile.close!
       true
     rescue StandardError => e
-      if e.message.match?(%r{password was incorrect}i) && (@resource[:password_fail_reset] == :true)
+      if e.message.match?(%r{password was incorrect}i) && (@resource[:password_fail_reset])
         # we have the wrong password for the keystore. so delete it if :password_fail_reset
         File.delete(@resource[:target])
       end
@@ -252,7 +252,7 @@ Puppet::Type.type(:java_ks).provide(:keytool) do
         '-file', certificate,
         '-keystore', @resource[:target]
       ]
-      cmd << '-trustcacerts' if @resource[:trustcacerts] == :true
+      cmd << '-trustcacerts' if @resource[:trustcacerts]
       tmpfile = password_file
       run_command(cmd, @resource[:target], tmpfile)
       tmpfile.close!

spec/acceptance/pkcs12_spec.rb
@@ -74,7 +74,7 @@ describe 'managing java pkcs12', unless: (os[:family] == 'sles' || (os[:family] 
         end
       end
     end
-  end # context 'with defaults'
+  end
 
   context 'with a different alias' do
     it 'creates a private key with chain' do
@@ -106,7 +106,7 @@ describe 'managing java pkcs12', unless: (os[:family] == 'sles' || (os[:family] 
         end
       end
     end
-  end # context 'with a different alias'
+  end
 
   context 'with a destkeypass' do
     command = if os[:family] == 'windows'
@@ -146,5 +146,5 @@ describe 'managing java pkcs12', unless: (os[:family] == 'sles' || (os[:family] 
         end
       end
     end
-  end # context 'with a destkeypass'
+  end
 end

commit 14e1a46665d0ea2a75ee1d375a4a4140da6c89b8

Fix boolean parameters
The unsafe rubocop fixes done in
189ac295bdd35196297476957c9f0b6e21e47829 broke the boolean parameters.

Before this commit, if you explicitly tried to set either of them to
`false`, they would actually be set to the symbol `:false` with is not
falsey so would act as if you'd set them to `true`.

lib/puppet/type/java_ks.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'puppet/parameter/boolean'
+
 Puppet::Type.newtype(:java_ks) do
   @doc = 'Manages the entries in a java keystore, and uses composite namevars to
   accomplish the same alias spread across multiple target keystores.'
@@ -132,12 +134,10 @@ Puppet::Type.newtype(:java_ks) do
       keystore. This cannot be used together with :password, but you must pass at least one of these parameters.'
   end
 
-  newparam(:password_fail_reset) do
+  newparam(:password_fail_reset, boolean: true, parent: Puppet::Parameter::Boolean) do
     desc "If the supplied password does not succeed in unlocking the
       keystore file, then delete the keystore file and create a new one.
       Default: false."
-
-    newvalues(true, false)
 
     defaultto false
   end
@@ -156,11 +156,9 @@ Puppet::Type.newtype(:java_ks) do
     end
   end
 
-  newparam(:trustcacerts) do
+  newparam(:trustcacerts, boolean: true, parent: Puppet::Parameter::Boolean) do
     desc "Certificate authorities aren't by default trusted so if you are adding a CA you need to set this to true.
      Defaults to :false."
-
-    newvalues(true, false)
 
     defaultto false
   end

spec/unit/puppet/type/java_ks_spec.rb
@@ -102,8 +102,22 @@ describe Puppet::Type.type(:java_ks) do
       expect(described_class.new(jks)[:name]).to eq(jks_resource[:name])
     end
 
-    it 'has false as the default value to :trustcacerts when parameter not provided' do
-      expect(described_class.new(jks_resource)[:trustcacerts]).to be_nil
+    it 'resource[:trustcacerts] is falsey when parameter not provided' do
+      expect(described_class.new(jks_resource)[:trustcacerts]).to be_falsey
+    end
+
+    it 'resource[:trustcacerts] is the boolean `false` when set to `false`' do
+      jks = jks_resource.dup
+      jks[:trustcacerts] = false
+
+      expect(described_class.new(jks)[:trustcacerts]).to be false
+    end
+
+    it 'resource[:trustcacerts] is the boolean `true` when set to `true`' do
+      jks = jks_resource.dup
+      jks[:trustcacerts] = true
+
+      expect(described_class.new(jks)[:trustcacerts]).to be true
     end
 
     it 'has :rsa as the default value for :private_key_type' do
@@ -175,8 +189,22 @@ describe Puppet::Type.type(:java_ks) do
       }.to raise_error(Puppet::Error, %r{length 6})
     end
 
-    it 'has :false value to :password_fail_reset when parameter not provided' do
-      expect(described_class.new(jks_resource)[:password_fail_reset]).to be_nil
+    it 'resource[:password_fail_reset] is falsey when parameter not provided' do
+      expect(described_class.new(jks_resource)[:password_fail_reset]).to be_falsey
+    end
+
+    it 'resource[:password_fail_reset] is the boolean `false` when parameter set to `false`' do
+      jks = jks_resource.dup
+      jks[:password_fail_reset] = false
+
+      expect(described_class.new(jks)[:password_fail_reset]).to be false
+    end
+
+    it 'resource[:password_fail_reset] is the boolean `true` when parameter set to `true`' do
+      jks = jks_resource.dup
+      jks[:password_fail_reset] = true
+
+      expect(described_class.new(jks)[:password_fail_reset]).to be true
     end
 
     it 'fails if :source_password is not provided for pkcs12 :storetype' do